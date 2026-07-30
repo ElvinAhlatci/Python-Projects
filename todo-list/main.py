b6a6a7a9489d10b1ccbc8ad2a929e7ae016cc4cc
Elvin Ahlatcı <elvinahltci@gmail.com>
2026-07-30 11:02:01 +0300

    Update main.py

--- todo-list/main.py
+++ todo-list/main.py
@@ -1,1 +1,22 @@
+gorevler = []
 
+while True:
+    print("1- Görev Ekle")
+    print("2- Görevleri Göster")
+    print("3- Çıkış")
+
+    secim = input("Seçiminiz: ")
+
+    if secim == "1":
+        gorev = input("Görev: ")
+        gorevler.append(gorev)
+
+    elif secim == "2":
+        for gorev in gorevler:
+            print(gorev)
+
+    elif secim == "3":
+        break
+
+    else:
+        print("Hatalı seçim!")
