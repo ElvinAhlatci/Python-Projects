5a5d514b43423bf6088a7c2a5146441f39e4b9ea
Elvin Ahlatcı <elvinahltci@gmail.com>
2026-07-30 11:20:21 +0300

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -5,19 +5,19 @@ islem = input("İşlem seçin (+, -, *, /): ")
 ikinci_sayi = int(input("İkinci sayıyı girin: "))
 
 if islem == "+":
-    print("Sonuç:", ilk_sayi + ikinci_sayi)
+    print("\nSonuç:", ilk_sayi + ikinci_sayi)
 
 elif islem == "-":
-    print("Sonuç:", ilk_sayi - ikinci_sayi)
+    print("\nSonuç:", ilk_sayi - ikinci_sayi)
 
 elif islem == "*":
-    print("Sonuç:", ilk_sayi * ikinci_sayi)
+    print("\nSonuç:", ilk_sayi * ikinci_sayi)
 
 elif islem == "/":
     if ikinci_sayi != 0:
-        print("Sonuç:", ilk_sayi / ikinci_sayi)
+        print("\nSonuç:", ilk_sayi / ikinci_sayi)
     else:
-        print("Hata: Bir sayı sıfıra bölünemez.")
+        print("\nHata: Bir sayı sıfıra bölünemez.")
 
 else:
-    print("Hatalı işlem seçtiniz.")
+    print("\nHatalı işlem seçtiniz.")
